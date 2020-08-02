4e0d129f8d2c80177710e3de824e1c0bc7fc105f
imseonju <wnwn37905@likelion.org>
2020-08-02 17:52:14 +0900

    layout

--- applysite/applysite/settings.py
+++ applysite/applysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'question.apps.QuestionConfig',
     'user.apps.UserConfig',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'user.CustomUser'
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "statics")
+)
--- applysite/question/views.py
+++ applysite/question/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def update(request):
+    return render(request, 'question/update.html')
