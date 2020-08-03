1d7cd6ccf2358596bb1cf3b22fbd64dae435daba
happysang <okglad@naver.com>
2020-08-03 18:11:34 +0900

    login

--- applysite/user/views.py
+++ applysite/user/views.py
@@ -5,25 +5,29 @@ from .models import CustomUser
 def sign_up(request):
     if request.method =='POST':
         username = request.POST['username']
+        class_code = request.POST['class_code']
         major = request.POST['major']
-        phone_number = request.POST['phone_number']
-        email = request.POST['email']
+        phone_number = request.POST['phone_number']  #추가내용
+        email = request.POST['email']  #추가내용
         pwd = request.POST['password']
         c_pwd = request.POST['check_password']
 
         if CustomUser.objects.filter(email=email).distinct():
-            return render(request, 'user/signUp.html', {'err' : '중복 아이디가 존재합니다.'})
+            return render(request, 'user/sign_up.html', {'err' : '중복 아이디가 존재합니다.'})
             if pwd != c_pwd:
-                return render(request, 'user/signUp.html', {'err' : '암호는 서로 일치해야 합니다.'})
+                return render(request, 'user/sign_up.html', {'err' : '암호는 서로 일치해야 합니다.'})
         customUser = CustomUser(
             username = username,
             email = email,
+            major = major,
+            phone_number = phone_number,
+            class_code = class_code,
             )
         customUser.set_password(pwd)
         customUser.save()
         return render(request, 'user/login.html') #return redirect('home')
     else:
-        return render(request, 'user/signUp.html')
+        return render(request, 'user/sign_up.html')
 
 def login(request):
     if request.method == "POST" :
