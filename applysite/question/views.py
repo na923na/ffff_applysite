13e150919b95922182a5d043b50d8ef475bc9ee2
ericjchoi99 <ericjchoi99@likelion.org>
2020-08-03 10:33:08 +0900

    new file:   question/templates/read.html 	new file:   question/templates/read_one.html 	modified:   question/views.py

--- applysite/question/views.py
+++ applysite/question/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
 
+
+def question_read(request):
+    questions = question.objects.all()
+    context = {'question', questions }
+    return render(request, 'question/read.html', context)
+
+def question_read_one(request, pk):
+    question = get_object_or_404(Question, pk = pk)
+    context = {'question' : question }
+    return render(request, 'board/read_one.html')
