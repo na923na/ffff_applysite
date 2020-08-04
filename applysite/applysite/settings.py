bbf47739ff0ba3fa7c4e69ff4926f3b8eacfddbd
imseonju <wnwn37905@likelion.org>
2020-08-04 17:08:20 +0900

    manager basic

--- applysite/applysite/settings.py
+++ applysite/applysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user.apps.UserConfig',
-    
+    'manager.apps.ManagerConfig',
 ]
 
 MIDDLEWARE = [
--- applysite/manager/urls.py
+++ applysite/manager/urls.py
@@ -18,11 +18,4 @@ from . import views
 
 urlpatterns = [
 
-    path('update/<int:pk>', views.question_update, name='question_update'),#선주
-    path('delete/<int:pk>', views.question_delete, name='question_delete'),#선주   
-    path('read/', views.question_read, name="question_read"), #question 안에 있는 read라서 question_read라 함 (최종인)
-    path('read/<int:pk>', views.question_read_one, name="question_read_one"), # question 안에 있는 read_one이라서 question_read_one 이라 함 / int:pk는 개별 게시글에 대한 것이므로 추가함 (최종인)
-    path('create/', views.question_create, name="question_create"),
-    path('pre_update/<int:pk>', views.pre_update, name='pre_update'),
-
 ]
