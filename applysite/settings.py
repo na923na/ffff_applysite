04babd66dfe22f403775b4e4007d91e126afe5e7
na923na <nanb_v@naver.com>
2020-08-21 16:41:34 +0900

    VV

--- applysite/settings.py
+++ applysite/settings.py
@@ -137,3 +137,5 @@ STATICFILES_DIR = (
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
